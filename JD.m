fb093a65747d19aa5a4904dd8a631cc9f63823a1
andylevalley <andylevalley@gmail.com>
2018-10-26 12:03:38 -0400

    Delete JD.m

--- JD.m
+++
@@ -1,8 +0,0 @@
-function jd = JD(yr,mo,d,h,mins,s)
-
-% Taken from VALLADO, algorithm 14
-
-sixty = 60;
-% UNLESS day contains a leap second, then sixty = 61;
-
-jd = 367*yr-floor(7*(yr+floor((mo+9)/12))/4)+floor(275*mo/9)+d+1721013.5+((s/sixty+mins)/60+h)/24;
